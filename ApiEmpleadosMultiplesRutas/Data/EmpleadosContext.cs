2de166810e0fa2025eb631b58b6c9f0b6b22c8aa
manu12perez <manu12perez@gmail.com>
2025-03-24 10:57:29 +0100

    Add project files.

---
+++ ApiEmpleadosMultiplesRutas/Data/EmpleadosContext.cs
@@ -0,0 +1,12 @@
+﻿using NugetApiModelsMpb;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiEmpleadosMultiplesRutas.Data
+{
+    public class EmpleadosContext: DbContext
+    {
+        public EmpleadosContext(DbContextOptions<EmpleadosContext> options) : base(options) { }
+
+        public DbSet<Empleado> Empleados { get; set; }
+    }
+}
